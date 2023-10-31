b992521b9456807acf4d6f7d85e1e09eaa07c1ea
mykolamytsyk <138970828+mykolamytsyk@users.noreply.github.com>
2023-10-31 23:02:02 +0200

    вввв

---
+++ practice.js
@@ -0,0 +1,45 @@
+// console.log("Hello Java");
+// console.info("Hello Java");
+// console.error("ffffff");
+// console.warn("ffffff");
+
+// let num_1 = 5;
+// let num_2 = 15;
+// let res = num_1 - num_2;
+
+// console.log("Result: " + (num_1 + num_2));
+
+// var num_3 = 10;
+// num_3--;
+// console.log("Result: " + num_3);
+
+// let str_1 = Number("12");
+// let str_2 = Number("2");
+// console.log("Result: " + (str_1 + str_2));
+
+// console.log("Math: " + Math.PI);
+// console.log("Math: " + Math.E);
+// console.log("Math: " + Math.min(4, 6, 2, -3, 0));
+// console.log("Math: " + Math.max(4, 666, 2, -3, 0));
+
+let number = 15;
+// if (number > 5) {
+//   console.log("Ok");
+// }
+
+// if (number != 15) {
+//   console.log("Ok");
+// } else {
+//   console.log("Else!");
+// }
+
+// можно сделать один элсе можно несколько елсе иф
+// if (number == 5) {
+//   console.log("Ok");
+// } else if (number < 10) {
+//   console.log("Ok");
+// } else if (number == 7) {
+//   console.log("7!");
+// } else if ((number = 15)) {
+//   console.log("15!");
+// }
